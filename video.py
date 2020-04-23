ac980598825998bce56bfc7fa2874edc1f7d5c87
BakondiTamas <bakondi1997@gmail.com>
2020-04-23 19:40:44 +0200

    Update

--- video.py
+++ video.py
@@ -4,13 +4,16 @@ cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (640,480))
 
+cap.set(3, 1280)
+cap.set(4, 720)
+
 print(cap.isOpened())
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
 
-        cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        cap.get(3)
+        cap.get(4)
 
         out.write(frame)
 
